c1b70b64c3d32fe76f98c078af17429254bf9d00
Felipe Vieira <flpvieira86@gmail.com>
2020-05-24 23:40:08 -0300

    adicionada as 62478264 classes

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-//variaveis
+// variaveis
 const inputTexto = document.getElementById('carta-texto');
 const botaoTexto = document.getElementById('criar-carta');
 const carta = document.getElementById('carta-gerada');
-
+let contador = document.getElementById('carta-contador');
 // functions
 
 function geraCarta() {
-  const array = inputTexto.value.split(' ');
-  console.log(array)
-  for (index = 0; index < array.length; index += 1) {
-    const span = document.createElement('span');
-    carta.appendChild(span);
-    span.innerText = array[index] + ' ';
+  carta.innerHTML = ''; // limpa a carta
+  const array = inputTexto.value.split(' '); // divide a string do input em um array de palavras
+  contador.innerText = array.length; // conta a quantidade de palavras no array 
+  for (let index = 0; index < array.length; index += 1) { // executa o for em cada palavra do array
+    const span = document.createElement('span'); // cria um span
+    carta.appendChild(span); // adiciona o span no paragrafo da carta
+    span.innerText = `${array[index]} `; // define o innertext do span com a palavra do array
   }
 }
 
-//listeners
+// listeners
 
-botaoTexto.addEventListener('click', geraCarta);
+botaoTexto.addEventListener('click', geraCarta); // listener que gera a carta
