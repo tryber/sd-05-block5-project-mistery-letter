b8e807e793c52283d365bb402645d8a9e91dbc0f
Felipe Vieira <flpvieira86@gmail.com>
2020-05-24 23:19:16 -0300

    adicionada função que gera a frase ao clicar no botão

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //variaveis
+const inputTexto = document.getElementById('carta-texto');
+const botaoTexto = document.getElementById('criar-carta');
+const carta = document.getElementById('carta-gerada');
+
+// functions
+
+function geraCarta() {
+  const array = inputTexto.value.split(' ');
+  console.log(array)
+  for (index = 0; index < array.length; index += 1) {
+    const span = document.createElement('span');
+    carta.appendChild(span);
+    span.innerText = array[index] + ' ';
+  }
+}
+
+//listeners
+
+botaoTexto.addEventListener('click', geraCarta);
