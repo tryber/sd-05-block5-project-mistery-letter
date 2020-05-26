a3facf7dfded8f949edfffc70a6fc371c98eb1f7
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-05-26 17:12:39 -0300

    Styles bugs fixed

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 const cardStyles = {
-  'styles' : ['newspaper', 'magazine1', 'magazine2'],
-  'sizes' : ['medium', 'big', 'reallybig'],
-  'rotations' : ['rotateleft', 'rotateright'],
-  'inclinations' : ['skewleft', 'skewright']
+  styles: ['newspaper', 'magazine1', 'magazine2'],
+  sizes: ['medium', 'big', 'reallybig'],
+  rotations: ['rotateleft', 'rotateright'],
+  inclinations: ['skewleft', 'skewright'],
 };
 const buildStyle = function () {
-  const style = cardStyles.styles[Math.round(Math.random() * cardStyles.styles.length)];
-  const size = cardStyles.sizes[Math.round(Math.random() * cardStyles.sizes.length)];
-  const rot = cardStyles.rotations[Math.round(Math.random() * cardStyles.rotations.length)];
-  const skew = cardStyles.inclinations[Math.round(Math.random() * cardStyles.inclinations.length)];
+  const style = cardStyles.styles[parseInt(Math.random() * cardStyles.styles.length)];
+  const size = cardStyles.sizes[parseInt(Math.random() * cardStyles.sizes.length)];
+  const rot = cardStyles.rotations[parseInt(Math.random() * cardStyles.rotations.length)];
+  const skew = cardStyles.inclinations[parseInt(Math.random() * cardStyles.inclinations.length)];
   return `${style} ${size} ${rot} ${skew} carta`;
 };
 const buildCards = function () {
   const letter = document.getElementById('carta-texto').value.split(' ');
   const messageContainer = document.getElementById('carta-gerada');
+  messageContainer.innerHTML = '';
   letter.forEach((word) => {
     const span = document.createElement('span');
     const card = messageContainer.appendChild(span);
     card.className = buildStyle();
     card.innerText = word;
   });
-}
+};
 window.onload = function () {
   this.document.getElementById('criar-carta').addEventListener('click', buildCards);
 };
