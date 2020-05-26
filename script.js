797c7cc0d74f3d6165f06315fecd835337ffabf9
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-05-26 18:35:46 -0300

    Start bonus

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*eslint no-extend-native: ["error", { "exceptions": ["Array"] }]*/
+/* eslint no-extend-native: ["error", { "exceptions": ["Array"] }] */
 Array.prototype.getRandom = function () {
   return this[Math.floor(Math.random() * this.length)];
 };
@@ -15,15 +15,20 @@ const buildStyle = function () {
   const skew = cardStyles.inclinations.getRandom();
   return `${style} ${size} ${rot} ${skew}`;
 };
+const changeStyle = function (element) {
+  element.target.className = buildStyle();
+};
 const buildCards = function () {
   const letter = document.getElementById('carta-texto').value.split(' ');
   const messageContainer = document.getElementById('carta-gerada');
+  document.getElementById('carta-contador').innerText = letter.length;
   messageContainer.innerHTML = '';
   letter.forEach((word) => {
     const span = document.createElement('span');
     const card = messageContainer.appendChild(span);
     card.className = buildStyle();
     card.innerText = word;
+    card.addEventListener('click', changeStyle);
   });
 };
 window.onload = function () {
