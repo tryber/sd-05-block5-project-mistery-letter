4c1c63906c3145627f7c3f29437683bd400de3b1
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-05-26 16:52:35 -0300

    Job finished

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-window.onload = function () {
-
+const cardStyles = {
+  'styles' : ['newspaper', 'magazine1', 'magazine2'],
+  'sizes' : ['medium', 'big', 'reallybig'],
+  'rotations' : ['rotateleft', 'rotateright'],
+  'inclinations' : ['skewleft', 'skewright']
+};
+const buildStyle = function () {
+  const style = cardStyles.styles[Math.round(Math.random() * cardStyles.styles.length)];
+  const size = cardStyles.sizes[Math.round(Math.random() * cardStyles.sizes.length)];
+  const rot = cardStyles.rotations[Math.round(Math.random() * cardStyles.rotations.length)];
+  const skew = cardStyles.inclinations[Math.round(Math.random() * cardStyles.inclinations.length)];
+  return `${style} ${size} ${rot} ${skew} carta`;
+};
+const buildCards = function () {
+  const letter = document.getElementById('carta-texto').value.split(' ');
+  const messageContainer = document.getElementById('carta-gerada');
+  letter.forEach((word) => {
+    const span = document.createElement('span');
+    const card = messageContainer.appendChild(span);
+    card.className = buildStyle();
+    card.innerText = word;
+  });
 }
+window.onload = function () {
+  this.document.getElementById('criar-carta').addEventListener('click', buildCards);
+};
