c3de92b5ff62ef3e6323888da63766d38d35779e
rfreitasbatista <rfreitasbatista@gmail.com>
2020-05-26 20:34:24 -0300

    esqueci dos commits

---
+++ script.js
@@ -0,0 +1,47 @@
+const estilo = ["newspaper", "magazine1", "magazine2"];
+const tamanho = ["medium", "big", "reallybig"];
+const rotacao = ["rotateleft", "rotateright"];
+const skew = ["skewleft", "skewright"];
+const fatherText = document.getElementById("carta-gerada");
+const inputText = document.getElementById("carta-texto");
+const inputBut = document.getElementById("criar-carta");
+
+function geraRandom(n) {
+  const random = Math.floor(Math.random() * n);
+  return random;
+}
+
+function geraEstilo() {
+  let estilogerado = estilo[geraRandom(3)];
+  let tamanhogerado = tamanho[geraRandom(3)];
+  let rotacaogerado = rotacao[geraRandom(2)];
+  let skewgerado = skew[geraRandom(2)];
+  let novoGrupoDeClass =
+    estilogerado + " " + tamanhogerado + " " + rotacaogerado + " " + skewgerado;
+  return novoGrupoDeClass;
+}
+
+function geraSpan(n) {
+  let novoSpan = document.createElement("span");
+  novoSpan.className = geraEstilo();
+  novoSpan.innerHTML = n;
+
+  fatherText.appendChild(novoSpan);
+}
+
+function removeOldSpan() {
+  let oldSpan = document.getElementsByTagName("span");
+  for (let i = oldSpan.length - 1; i >= 0; i--) {
+    fatherText.removeChild(oldSpan[i]);
+  }
+}
+
+function splitter() {
+  removeOldSpan();
+  const texto = inputText.value;
+  const textoSplit = texto.split(" ");
+  for (let i = 0; i < textoSplit.length; i++) {
+    geraSpan(textoSplit[i]);
+  }
+}
+inputBut.addEventListener("click", splitter);
