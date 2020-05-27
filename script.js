6619d9d103e3964931bdce428f15b81027cd03c7
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-05-27 18:27:22 -0300

    Funçao para alterar os estilos de cada um dos spans aleatoriamente

--- script.js
+++ script.js
@@ -22,9 +22,53 @@ function criarElementosCarta() {
   }
 }
 
+function atribuirClasseEstilo() {
+  // Sorteia uma clasee para cada palavra
+  for (let i = 0; i < qtdePalavras; i += 1) {
+    // Sorteio Estilos
+    const numEstilo = Math.floor(Math.random() * 3);
+    const estilo = classesEstilos[numEstilo];
+    let spanPalavra = document.querySelectorAll('#carta-gerada span');
+    spanPalavra[i].classList.add(estilo);
+  }
+}
+function atribuirClasseTamanho() {
+  // Sorteia uma classe para cada palavra
+  for (let i = 0; i < qtdePalavras; i += 1) {
+    // Sorteio Tamanho
+    const numTamanho = Math.floor(Math.random() * 3);
+    const tamanho = classesTamaho[numTamanho];
+    let spanPalavra = document.querySelectorAll('#carta-gerada span');
+    spanPalavra[i].classList.add(tamanho);
+  }
+}
+function atribuirClasseRotacao() {
+  // Sorteia uma classe para cada palavra
+  for (let i = 0; i < qtdePalavras; i += 1) {
+    // Sorteio Rotacao
+    const numRotacao = Math.floor(Math.random() * 2);
+    const rotacao = classesRotacao[numRotacao];
+    let spanPalavra = document.querySelectorAll('#carta-gerada span');
+    spanPalavra[i].classList.add(rotacao);
+  }
+}
+function atribuirClasseInclinacao() {
+  // Sorteia uma clasee para cada palavra
+  for (let i = 0; i < qtdePalavras; i += 1) {
+    // Sorteio Inclinação
+    const numInclinacao = Math.floor(Math.random() * 2);
+    const inclinacao = classesInclinacao[numInclinacao];
+    let spanPalavra = document.querySelectorAll('#carta-gerada span');
+    spanPalavra[i].classList.add(inclinacao);
+  }
+}
+
 const botaoGerar = document.getElementById('criar-carta');
 function criarCarta() {
   criarElementosCarta();
+  atribuirClasseEstilo();
+  atribuirClasseTamanho();
+  atribuirClasseRotacao();
+  atribuirClasseInclinacao();
 }
 botaoGerar.addEventListener('click', criarCarta);
-
