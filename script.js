fb336fe46cfd287687ad327caf0ed16826d26dc2
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-05-27 19:41:30 -0300

    code climate little issues

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//SECTION
-
 // 1 - Deve haver um input com o id="carta-texto"
 // onde o usuário poderá digitar o conteúdo da carta.
 const inputCartaTexto = document.getElementById('carta-texto');
@@ -15,17 +13,16 @@ const buttonCriarCarta = document.getElementById('criar-carta');
 // * Cada palavra deve aparecer dentro de uma tag `span`.
 // * As tags `span` devem ser adicionadas como filha do parágrafo que possui o id `carta-gerada`.
 
-//SECTION Gerar carta
+//  SECTION Gerar carta
 
 function gerarCarta() {
   const inputCartaTextoValue = inputCartaTexto.value;
   const splitInputCartaTextoValue = inputCartaTextoValue.split(' ');
-  console.log(splitInputCartaTextoValue);
-  for (let index = 0; index < splitInputCartaTextoValue.length; index++) {
+  for (let index = 0; index < splitInputCartaTextoValue.length; index += 1) {
     const element = splitInputCartaTextoValue[index];
-    let span = document.createElement('span');
+    const span = document.createElement('span');
     span.innerHTML = `${element}`;
-    let classLibrary = {
+    const classLibrary = {
       1: 'newspaper',
       2: 'magazine1',
       3: 'magazine2',
@@ -37,15 +34,15 @@ function gerarCarta() {
       9: 'skewleft',
       10: 'skewright',
     };
-    
-    let randomClass = classLibrary[Math.ceil(Math.random() * 11)];
+
+    const randomClass = classLibrary[Math.ceil(Math.random() * 11)];
     span.className = randomClass;
     outputCartaGerada.appendChild(span);
   }
 }
 
 buttonCriarCarta.addEventListener('click', gerarCarta);
-//same function, on ENTER keyup
+//  same function, on ENTER keyup
 inputCartaTexto.addEventListener('keyup', function (event) {
   if (event.keyCode === 13) {
     gerarCarta();
