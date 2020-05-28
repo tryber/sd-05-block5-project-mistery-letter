47b0183a926e52f95fa8d204e4f5672cf43cbf0c
pedrohcalado <pedrocalado22@gmail.com>
2020-05-28 08:39:19 -0300

    Add random classes to the span

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// classes groups
+const styleGroup = ['newspaper', 'magazine1', 'magazine2'];
+const sizeGroup = ['medium', 'big', 'reallybig'];
+const rotationGroup = ['rotateleft', 'rotateright'];
+const inclinationGroup = ['skewleft', 'skewright'];
+
 // create span tags and add it to the p tag once the button is clicked
 const button = document.querySelector('button');
 const text = document.querySelector('input');
@@ -8,6 +14,10 @@ button.addEventListener('click', function () {
   for (let i = 0; i < words.length; i += 1) {
     const span = document.createElement('span');
     span.innerHTML = words[i];
+    span.classList.add(styleGroup[Math.floor(Math.random() * 3)]);
+    span.classList.add(sizeGroup[Math.floor(Math.random() * 3)]);
+    span.classList.add(rotationGroup[Math.floor(Math.random() * 2)]);
+    span.classList.add(inclinationGroup[Math.floor(Math.random() * 2)]);
     letter.appendChild(span);
   }
 });
