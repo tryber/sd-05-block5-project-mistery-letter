09cc502d38fcc5b1527d5ce674000b3975e928f4
pauloricardoz <trybe.przt@gmail.com>
2020-05-29 14:48:56 -0300

    letter ready

---
+++ script.js
@@ -0,0 +1,40 @@
+const grupoEstilo = ['newspaper', 'magazine1', 'magazine2']
+const grupoTamanho = ['medium', 'big', 'reallybig']
+const grupoRotação = ['rotateleft', 'rotateright']
+const GrupoInclinação = ['skewleft', 'skewright']
+const grupos = [grupoEstilo, grupoTamanho, grupoRotação, GrupoInclinação]
+function rnd(size) {
+  let num = Math.floor(Math.random() * (size - 1) + 0.8)
+  return num > size ? size : num;
+}
+
+function sorteio() {
+  let classe = "";
+  for (let i = 0; i < grupos.length; i++) {
+    let teste = rnd(grupos[i].length)
+    classe += grupos[i][teste] + ' ';
+  }
+  return classe
+}
+
+function vamos() {
+  let texto = document.getElementById('carta-texto').value;
+
+  console.log(texto)
+  texto = texto.split(" ");
+
+  console.log(texto)
+  for (let palavras = 0; palavras < texto.length; palavras++) {
+    const classes = sorteio();
+    console.log(classes)
+    const elemento = document.createElement('span');
+    elemento.className = classes;
+    elemento.innerHTML=texto[palavras]
+    document.getElementById('carta-gerada').appendChild(elemento);
+  }
+  return "ok"
+}
+const botao = document.getElementById('criar-carta');
+botao.addEventListener('click', vamos);
+
+
