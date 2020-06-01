e972da081dfcdb4cf9f8a13e85288c68cdee7316
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-05-31 23:39:21 -0300

    até o item 14 feito

---
+++ script.js
@@ -0,0 +1,18 @@
+const posBotaoCriarCarta = document.getElementById('criar-carta');
+
+function imprimeCarta(){
+  const conteudoTexto = document.getElementById('carta-texto').value;
+  const vetorAuxiliar = conteudoTexto.split(" ");
+  const ultimoParagrafo = document.getElementById('carta-gerada');
+  console.log(ultimoParagrafo);
+
+  for (let i = 0; i<vetorAuxiliar.length; i += 1) {
+    const criaTagSpan = document.createElement('span');
+    criaTagSpan.innerText = vetorAuxiliar[i];
+    console.log(criaTagSpan);
+    ultimoParagrafo.appendChild(criaTagSpan);
+  }
+
+}
+
+posBotaoCriarCarta.addEventListener('click',imprimeCarta);
