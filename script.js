1a8df4cde1acc0086530c8ee4d3f606aeb9eaac9
RenataNovais <rnovaiscs@gmail.com>
2020-07-11 15:08:34 -0300

    fixing cc

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const getLetter = document.getElementById('criar-carta');
 const letterMade = document.getElementById('carta-gerada');
 const letterInput = document.getElementById('carta-texto');
 
+// const estilo = ['newspaper', 'magazine1', 'magazine2'];
+// const tamanho = ['medium', 'big', 'reallybig'];
+// const rotacao = ['rotateleft', 'rotateright'];
+// const inclinacao = ['skewleft', 'skewright'];
+
 function printLetter() {
   const letterText = letterInput.value;
   const stringsArr = letterText.split(' ');
