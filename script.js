59d4968ee6c971fa36ad80339287a18fe0c8fd98
RenataNovais <rnovaiscs@gmail.com>
2020-07-11 14:56:29 -0300

    fixing cc

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const letterInput = document.getElementById('carta-texto');
 function printLetter() {
   const letterText = letterInput.value;
   const stringsArr = letterText.split(' ');
-  return stringsArr.forEach(word => {
+  return stringsArr.forEach((word) => {
     const spanWord = document.createElement('span');
     spanWord.innerHTML = word;
     letterMade.appendChild(spanWord);
