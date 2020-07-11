176e96625892cae6780104752e26cf1fe8a9a241
RenataNovais <rnovaiscs@gmail.com>
2020-07-11 14:53:25 -0300

    first commit - requirements 1 to 3 :rocket:

---
+++ script.js
@@ -0,0 +1,15 @@
+const getLetter = document.getElementById('criar-carta');
+const letterMade = document.getElementById('carta-gerada');
+const letterInput = document.getElementById('carta-texto');
+
+function printLetter() {
+  const letterText = letterInput.value;
+  const stringsArr = letterText.split(' ');
+  return stringsArr.forEach(word => {
+    const spanWord = document.createElement('span');
+    spanWord.innerHTML = word;
+    letterMade.appendChild(spanWord);
+  });
+}
+
+getLetter.addEventListener('click', printLetter);
