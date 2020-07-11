2bc6b96d4c0be39d53510f683fd203aa208738da
RenataNovais <rnovaiscs@gmail.com>
2020-07-11 15:33:20 -0300

    adding style to letter

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ const getLetter = document.getElementById('criar-carta');
 const letterMade = document.getElementById('carta-gerada');
 const letterInput = document.getElementById('carta-texto');
 
-// const estilo = ['newspaper', 'magazine1', 'magazine2'];
-// const tamanho = ['medium', 'big', 'reallybig'];
-// const rotacao = ['rotateleft', 'rotateright'];
-// const inclinacao = ['skewleft', 'skewright'];
+const estilo = ['newspaper', 'magazine1', 'magazine2'];
+const tamanho = ['medium', 'big', 'reallybig'];
+const rotacao = ['rotateleft', 'rotateright'];
+const inclinacao = ['skewleft', 'skewright'];
+
+
+const ranNum = (n) => Math.floor(Math.random() * n);
+
+function randomClasses() {
+  return `${estilo[ranNum(3)]} ${tamanho[ranNum(3)]} ${rotacao[ranNum(2)]} ${inclinacao[ranNum(2)]}}`;
+}
 
 function printLetter() {
   const letterText = letterInput.value;
@@ -13,6 +20,7 @@ function printLetter() {
   return stringsArr.forEach((word) => {
     const spanWord = document.createElement('span');
     spanWord.innerHTML = word;
+    spanWord.className = randomClasses();
     letterMade.appendChild(spanWord);
   });
 }
